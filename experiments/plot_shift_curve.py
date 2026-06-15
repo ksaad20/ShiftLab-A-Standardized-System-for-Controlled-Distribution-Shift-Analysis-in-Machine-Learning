69d70a8c2e8a13ca8396591a6d16811b3877dc50
Kazi Saad Asif <62234655+ksaad20@users.noreply.github.com>
2026-06-15 12:29:08 +0600

    Add plotting functionality for shift results

--- experiments/plot_shift_curve.py
+++ experiments/plot_shift_curve.py
@@ -1,1 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+data = np.load("shift_results.npy", allow_pickle=True)
+
+sigmas = [x[0] for x in data]
+accs = [x[1] for x in data]
+
+plt.plot(sigmas, accs, marker='o')
+plt.xlabel("Shift magnitude (sigma)")
+plt.ylabel("Accuracy")
+plt.title("ShiftLab: Performance under controlled shift")
+plt.show()
